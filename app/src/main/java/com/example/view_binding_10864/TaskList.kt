55710073c58463257004b43d96feafba2bbbeb14
Papoy <imaderexy@gmail.com>
2022-09-15 08:11:03 +0700

    GD4 View Binding

---
+++ app/src/main/java/com/example/view_binding_10864/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10864
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur","Cek HP Nyari Masalah"),
+        Task("Chapter 2", "Say Selamat Pagi","Nyari Masalah Part II"),
+        Task("Chapter 3", "Login ML","Adu Mekanik Sini Maju Boss"),
+        Task("Chapter 4", "Makan ","Makan Hati"),
+        Task("Chapter 5", "Bobo","Mimpiin Kamuuuuu")
+    )
+}
